493e5ac24f269fd8aee96b7904c4e5c119125ebf
abel aime <abel.tchide-saa.1@ulaval.ca>
2019-11-20 12:34:00 -0500

    modif50

--- quoridor.py
+++ quoridor.py
@@ -1,7 +1,8 @@
+"""
 class Quoridor:
 
     def __init__(self, joueurs, murs=None):
-        """
+    
         Initialiser une partie de Quoridor avec les joueurs et les murs spécifiés, 
         en s'assurant de faire une copie profonde de tout ce qui a besoin d'être copié.
 
@@ -16,7 +17,7 @@ class Quoridor:
         :param murs: un dictionnaire contenant une clé 'horizontaux' associée à la liste des
         positions (x, y) des murs horizontaux, et une clé 'verticaux' associée à la liste des
         positions (x, y) des murs verticaux. Par défaut, il n'y a aucun mur placé sur le jeu.
-
+        
         :raises QuoridorError: si joueurs n'est pas itérable.
         :raises QuoridorError: si l'itérable de joueurs en contient plus de deux.
         :raises QuoridorError: si le nombre de murs qu'un joueur peut placer est >10, ou négatif.
@@ -24,7 +25,7 @@ class Quoridor:
         :raises QuoridorError: si murs n'est pas un dictionnaire lorsque présent.
         :raises QuoridorError: si le total des murs placés et plaçables n'est pas égal à 20.
         :raises QuoridorError: si la position d'un mur est invalide.
-        """
+        
         self.joueur1 = joueurs[0]
         self.joueur2 = joueurs[1]
         self.murs = murs
@@ -59,17 +60,17 @@ class Quoridor:
                 raise QuorridorError
             if m[1] < 1 or m[1] > 8:
                 raise QuorridorError
-        
+       
 
         
         
     def __str__(self):
-        """
+        
         Produire la représentation en art ascii correspondant à l'état actuel de la partie. 
         Cette représentation est la même que celle du TP précédent.
 
         :returns: la chaîne de caractères de la représentation.
-        """
+        
         deb = 'Légende: 1='+str(self.état_partie['joueurs'][0]['nom'])+' 2=automate'+'\n'+'   '+35*'-'+'\n'
     sui = ''
     for i in range(8):
@@ -88,7 +89,7 @@ class Quoridor:
     return deb + ''.join(tot)
 
     def déplacer_jeton(self, joueur, position):
-        """
+    
         Pour le joueur spécifié, déplacer son jeton à la position spécifiée.
 
         :param joueur: un entier spécifiant le numéro du joueur (1 ou 2).
@@ -96,10 +97,10 @@ class Quoridor:
         :raises QuoridorError: le numéro du joueur est autre que 1 ou 2.
         :raises QuoridorError: la position est invalide (en dehors du damier).
         :raises QuoridorError: la position est invalide pour l'état actuel du jeu.
-        """
+    
 
     def état_partie(self):
-        """
+    
         Produire l'état actuel de la partie.
 
         :returns: une copie de l'état actuel du jeu sous la forme d'un dictionnaire:
@@ -124,10 +125,10 @@ class Quoridor:
         est relative à leur coin inférieur gauche. Par convention, un mur horizontal se
         situe entre les lignes y-1 et y, et bloque les colonnes x et x+1. De même, un
         mur vertical se situe entre les colonnes x-1 et x, et bloque les lignes x et x+1.
-        """
+    
 
     def jouer_coup(self, joueur):
-        """
+        
         Pour le joueur spécifié, jouer automatiquement son meilleur coup pour l'état actuel 
         de la partie. Ce coup est soit le déplacement de son jeton, soit le placement d'un 
         mur horizontal ou vertical.
@@ -135,17 +136,17 @@ class Quoridor:
         :param joueur: un entier spécifiant le numéro du joueur (1 ou 2).
         :raises QuoridorError: le numéro du joueur est autre que 1 ou 2.
         :raises QuoridorError: la partie est déjà terminée.
-        """
+        
 
     def partie_terminée(self):
-        """
+    
         Déterminer si la partie est terminée.
 
         :returns: le nom du gagnant si la partie est terminée; False autrement.
-        """
+        
 
     def placer_mur(self, joueur: int, position: tuple, orientation: str):
-        """
+        
         Pour le joueur spécifié, placer un mur à la position spécifiée.
 
         :param joueur: le numéro du joueur (1 ou 2).
@@ -155,6 +156,82 @@ class Quoridor:
         :raises QuoridorError: un mur occupe déjà cette position.
         :raises QuoridorError: la position est invalide pour cette orientation.
         :raises QuoridorError: le joueur a déjà placé tous ses murs.
-        """
+    
 class QuorridorError(Exception):
+"""
+
+
+import networkx as nx
+
+
+def construire_graphe(joueurs, murs_horizontaux, murs_verticaux):
+    """
+    Crée le graphe des déplacements admissibles pour les joueurs.
+
+    :param joueurs: une liste des positions (x,y) des joueurs.
+    :param murs_horizontaux: une liste des positions (x,y) des murs horizontaux.
+    :param murs_verticaux: une liste des positions (x,y) des murs verticaux.
+    :returns: le graphe bidirectionnel (en networkX) des déplacements admissibles.
+    """
+    état = {"joueurs": [{"nom": "idul", "murs": 7, "pos": [5, 6]},{"nom": "automate", "murs": 3, "pos": [5, 7]}],
+    "murs": {"horizontaux": [[4, 4], [2, 6], [3, 8], [5, 8], [7, 8]],"verticaux": [[6, 2], [4, 4], [2, 5], [7, 5], [7, 7]]}}
+    graphe = nx.DiGraph()
+    joueurs =[tuple(état["joueurs"][0]["pos"]), tuple(état["joueurs"][1]["pos"])]
+    murs_horizontaux = état["murs"]["horizontaux"]
+    murs_verticaux = état["murs"]["verticaux"]
+
+    # pour chaque colonne du damier
+    for x in range(1, 10):
+        # pour chaque ligne du damier
+        for y in range(1, 10):
+            # ajouter les arcs de tous les déplacements possibles pour cette tuile
+            if x > 1:
+                graphe.add_edge((x, y), (x-1, y))
+            if x < 9:
+                graphe.add_edge((x, y), (x+1, y))
+            if y > 1:
+                graphe.add_edge((x, y), (x, y-1))
+            if y < 9:
+                graphe.add_edge((x, y), (x, y+1))
+
+    # retirer tous les arcs qui croisent les murs horizontaux
+    for x, y in murs_horizontaux:
+        graphe.remove_edge((x, y-1), (x, y))
+        graphe.remove_edge((x, y), (x, y-1))
+        graphe.remove_edge((x+1, y-1), (x+1, y))
+        graphe.remove_edge((x+1, y), (x+1, y-1))
+
+    # retirer tous les arcs qui croisent les murs verticaux
+    for x, y in murs_verticaux:
+        graphe.remove_edge((x-1, y), (x, y))
+        graphe.remove_edge((x, y), (x-1, y))
+        graphe.remove_edge((x-1, y+1), (x, y+1))
+        graphe.remove_edge((x, y+1), (x-1, y+1))
+
+    # retirer tous les arcs qui pointent vers les positions des joueurs
+    # et ajouter les sauts en ligne droite ou en diagonale, selon le cas
+    for joueur in map(tuple, joueurs):
+
+        for prédécesseur in list(graphe.predecessors(joueur)):
+            graphe.remove_edge(prédécesseur, joueur)
+
+            # si admissible, ajouter un lien sauteur
+            successeur = (2*joueur[0]-prédécesseur[0], 2*joueur[1]-prédécesseur[1])
+
+            if successeur in graphe.successors(joueur) and successeur not in joueurs:
+                # ajouter un saut en ligne droite
+                graphe.add_edge(prédécesseur, successeur)
+
+            else:
+                # ajouter les liens en diagonal
+                for successeur in list(graphe.successors(joueur)):
+                    if prédécesseur != successeur and successeur not in joueurs:
+                        graphe.add_edge(prédécesseur, successeur)
+
+    # ajouter les noeuds objectifs des deux joueurs
+    for x in range(1, 10):
+        graphe.add_edge((x, 9), 'B1')
+        graphe.add_edge((x, 1), 'B2')
+
+    return graphe
 
